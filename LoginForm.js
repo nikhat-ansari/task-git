a7f09f7d34b85340fb15051eaa5b55c3837dc4e5
nikhat-ansari <163503627+nikhat-ansari@users.noreply.github.com>
2026-07-09 18:48:22 +0530

    Added username field

--- LoginForm.js
+++ LoginForm.js
@@ -18,6 +18,17 @@ function LoginForm() {
         </div>
 
         <br />
+        <div>
+          <label>Password:</label>
+          <br />
+          <input
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+          />
+        </div>
+
+        <br />
 
         <button type="submit">Login</button>
       </form>
