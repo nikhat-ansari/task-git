11e1bb7f8eb91952dcd87bda837e1ee515150a82
nikhat-ansari <163503627+nikhat-ansari@users.noreply.github.com>
2026-07-09 18:36:36 +0530

    Added LoginForm component

---
+++ LoginForm.js
@@ -0,0 +1,28 @@
+function LoginForm() {
+  return (
+    <div>
+      <h2>User Login</h2>
+
+      <form>
+        <div>
+          <label>Username:</label>
+          <br />
+          <input type="username" placeholder="Enter your name" name="Nikhat" />
+        </div>
+        <br />
+
+        <div>
+          <label>Email:</label>
+          <br />
+          <input type="email" placeholder="Enter your email" name="email" />
+        </div>
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
